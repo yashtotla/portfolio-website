e4337848b49c00284fd74160123ef254734edc71
Yash Totla <yash.totla@oracle.com>
2021-11-24 13:40:09 +0530

    landing page

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,20 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+	<Section row nopadding>
+		<LeftSection>
+			<SectionTitle main center>
+				Welcome To <br />
+				My Personal Portfolio
+			</SectionTitle>
+			<SectionText>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla suscipit ornare dui, nec efficitur magna ultrices id. Aenean iaculis ex sem, vel varius dolor semper sed. Curabitur ac fermentum sem. Integer vitae viverra massa.
+			</SectionText>
+			<Button onClick={() => window.location = '#'}>
+				Learn More
+			</Button>
+		</LeftSection>
+	</Section>
 );
 
 export default Hero;
--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,12 +1,53 @@
 import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
+import { DiDatabase, DiFirebase, DiMysql, DiPython, DiReact, DiZend } from 'react-icons/di';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
-const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+const Technologies = () => (
+	<Section id='tech'>
+		<SectionDivider />
+		<br/>
+		<SectionTitle>Technologies</SectionTitle>
+		<SectionText>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla suscipit ornare dui, nec efficitur magna ultrices id. Aenean iaculis ex sem, vel varius dolor semper sed.
+		</SectionText>
+		<List>
+			<ListItem>
+				<DiReact size='3rem' />
+				<ListContainer>
+					<ListTitle>Front End</ListTitle>
+					<ListParagraph>
+						Experience with <br/>
+						React.js <br/>
+						Knockout.js
+					</ListParagraph>
+				</ListContainer>
+			</ListItem>
+			<ListItem>
+				<DiReact size='3rem' />
+				<ListContainer>
+					<ListTitle>Machine Learning</ListTitle>
+					<ListParagraph>
+						Experience with <br/>
+						Python <br />
+						Tensorflow
+					</ListParagraph>
+				</ListContainer>
+			</ListItem>
+			<ListItem>
+				<DiDatabase size='3rem' />
+				<ListContainer>
+					<ListTitle>Database</ListTitle>
+					<ListParagraph>
+						Experience with <br/>
+						Google Bigquery <br/>
+						Neo4j <br/>
+						Memgraph 
+					</ListParagraph>
+				</ListContainer>
+			</ListItem>
+		</List>
+	</Section >
 );
 
 export default Technologies;
